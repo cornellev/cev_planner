adb919401d712d76335e4bc5d8fc2b9af9aa0da4
Sidharth Rao <sidharthmrao@gmail.com>
2025-03-12 01:24:39 -0400

    Adjusted cost values.

--- src/local_planner/mpc.cpp
+++ src/local_planner/mpc.cpp
@@ -137,7 +137,7 @@ namespace cev_planner::local_planner {
         // }
 
         std::vector<State> path = this->decompose(*this->temp_start, x, this->dt);
-        return 8 * path_obs_cost(path) + 7 * path_waypoints_cost(path);
+        return 5000 * path_obs_cost(path) + 7 * path_waypoints_cost(path);
     }
 
     double MPC::objective_function(const std::vector<double>& x, std::vector<double>& grad,
